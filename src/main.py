964cb122dc036a8046f469226f97453543fe33c5
sureshrajuvetukri <vetukuri.sureshraju@gmail.com>
2024-12-23 06:31:32 +0530

    add docker file

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
-from flask import Flask, request, jsonify
-import pickle
+# from flask import Flask, request, jsonify
+# import pickle
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json(force=True)
-    model = pickle.load(open('src/models/model.pkl', 'rb'))
-    prediction = model.predict([data['features']])
-    return jsonify({'prediction': prediction.tolist()})
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     data = request.get_json(force=True)
+#     model = pickle.load(open('src/models/model.pkl', 'rb'))
+#     prediction = model.predict([data['features']])
+#     return jsonify({'prediction': prediction.tolist()})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=80)
--- src/test/test_model.py
+++ src/test/test_model.py
@@ -1,9 +1,9 @@
-import pytest
-import requests
+# import pytest
+# import requests
 
-def test_predict():
-    url = "http://localhost:80/predict"
-    data = {"features": [1.0, 2.0, 3.0, 4.0]}
-    response = requests.post(url, json=data)
-    assert response.status_code == 200
-    assert 'prediction' in response.json()
+# def test_predict():
+#     url = "http://localhost:80/predict"
+#     data = {"features": [1.0, 2.0, 3.0, 4.0]}
+#     response = requests.post(url, json=data)
+#     assert response.status_code == 200
+#     assert 'prediction' in response.json()
